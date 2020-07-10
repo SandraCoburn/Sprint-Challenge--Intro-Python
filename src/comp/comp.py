3db1f56982bb97e7ad4a5f45827e838b1f7bec39
Sandra Coburn <sandra_coburn@hotmail.com>
2020-07-10 10:48:15 -0700

    Fixed upper case comprehension to pass

--- src/comp/comp.py
+++ src/comp/comp.py
@@ -63,7 +63,7 @@ print(f)
 # list, except with all the names uppercase and the ages with 5 added to them.
 # The "humans" list should be unmodified.
 print("All names uppercase:")
-g = [(name.name.upper(), name.age + 5 ) for name in humans]
+g = [Human(name.name.upper(), name.age + 5 ) for name in humans]
 print(g)
 
 # Write a list comprehension that contains the square root of all the ages.
